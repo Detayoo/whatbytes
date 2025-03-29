87f3e3a2be39c41d79e2d61dac404b168f739fbb
Tayo <teeydigba@gmail.com>
2025-03-29 08:16:36 +0100

    added header tag to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between border-b border-b-gray-200 items-center min-h-[80px] w-full px-4">
+    <header className="flex justify-between border-b border-b-gray-200 items-center min-h-[80px] w-full px-4">
       <p className="text-black text-[25px] font-semibold">WhatBytes</p>
       <div className="flex gap-x-3 items-center rounded-[6px] border border-gray-200 px-3 py-2">
         <Image
@@ -14,6 +14,6 @@ export const Header = () => {
         />
         <p className="text-black font-semibold">Tayo Adedigba</p>
       </div>
-    </div>
+    </header>
   );
 };
