e02bcd69fff56a431cf65d8f7c9e787c5093ae41
Tayo <teeydigba@gmail.com>
2025-03-29 08:15:48 +0100

    fixed syllabus analysis

--- src/components/SyllabusAnalysis.tsx
+++ src/components/SyllabusAnalysis.tsx
@@ -8,49 +8,57 @@ export const SyllabusAnalysis = () => {
         <li>
           <div className="flex justify-between mb-2">
             <span className="text-gray-600">HTML Tools, Forms, History</span>
-            <span className="text-gray-600">80%</span>
           </div>
-          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div
-              className="bg-primary h-2.5 rounded-full"
-              style={{ width: "80%" }}
-            ></div>
+          <div className="flex justify-between items-center gap-x-4">
+            <div className="w-[80%] bg-gray-200 rounded-full h-2.5">
+              <div
+                className="bg-primary h-2.5 rounded-full"
+                style={{ width: "80%" }}
+              ></div>
+            </div>
+            <span className="text-primary font-semibold">80%</span>
           </div>
         </li>
         <li>
           <div className="flex justify-between mb-2">
             <span className="text-gray-600">Tags & References in HTML</span>
-            <span className="text-gray-600">60%</span>
           </div>
-          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div
-              className="bg-warning h-2.5 rounded-full"
-              style={{ width: "60%" }}
-            ></div>
+          <div className="flex justify-between items-center gap-x-4">
+            <div className="w-[80%] bg-gray-200 rounded-full h-2.5">
+              <div
+                className="bg-warning h-2.5 rounded-full"
+                style={{ width: "60%" }}
+              ></div>
+            </div>
+            <span className="text-warning font-semibold">60%</span>
           </div>
         </li>
         <li>
           <div className="flex justify-between mb-2">
             <span className="text-gray-600">Tables & References in HTML</span>
-            <span className="text-gray-600">24%</span>
           </div>
-          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div
-              className="bg-danger h-2.5 rounded-full"
-              style={{ width: "24%" }}
-            ></div>
+          <div className="flex justify-between items-center gap-x-4">
+            <div className="w-[80%] bg-gray-200 rounded-full h-2.5">
+              <div
+                className="bg-danger h-2.5 rounded-full"
+                style={{ width: "24%" }}
+              ></div>
+            </div>
+            <span className="text-danger font-semibold">24%</span>
           </div>
         </li>
         <li>
           <div className="flex justify-between mb-2">
             <span className="text-gray-600">Tables & CSS Basics</span>
-            <span className="text-gray-600">96%</span>
           </div>
-          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div
-              className="bg-success h-2.5 rounded-full"
-              style={{ width: "96%" }}
-            ></div>
+          <div className="flex justify-between items-center gap-x-4">
+            <div className="w-[80%] bg-gray-200 rounded-full h-2.5">
+              <div
+                className="bg-success h-2.5 rounded-full"
+                style={{ width: "96%" }}
+              ></div>
+            </div>
+            <span className="text-success font-semibold">96%</span>
           </div>
         </li>
       </ul>
